d1f6b785da9081d0309700e2193d8e94cc0c3589
Fabian Deutsch <fabiand@fedoraproject.org>
2012-05-31 14:53:54 +0200

    Add jenkins job example
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

---
+++ data/jenkins_job_example.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -xe
+#
+# Example script for jenkins
+#
+# Pass the following env vars:
+
+[[ -z $IGORCLIENTURL ]] && exit 1
+[[ -z $APIURL ]] && exit 1
+[[ -z $BASENAMEPREFIX ]] && exit 1
+[[ -z $ARTIFACTSARCHIVE ]] && exit 1
+
+# This is an artifact from a previous job
+ISONAME=$(ls *.iso)
+
+echo Using ISO '$ISONAME'
+
+wget --quiet "${IGORCLIENTURL}" -O "igorclient.sh"
+
+export BASENAME="${BASENAMEPREFIX}${BUILD_TAG}"       # profile and distro name are derived from BASENAME
+export PROFILENAME="$BASENAME-profile"
+
+# Create cobbler distro and profile
+bash ./igorclient.sh extra_profile_add "$BASENAME" "$ISONAME"
+
+# Create igor job
+bash ./igorclient.sh submit example "$PROFILENAME" ahost
+export $(bash ./igorclient.sh cookie)
+[[ -z $IGORCOOKIE ]] && { echo No Igor cookie ; return 0 ; }
+# ... start and wait to reach some endstate
+bash ./igorclient.sh start
+bash ./igorclient.sh wait_state "aborted|failed|timedout|done"
+
+LAST_STATE=$(bash ./igorclient.sh state)
+
+# get artifacts archive
+bash ./igorclient.sh artifacts $ARTIFACTSARCHIVE
+bash ./igorclient.sh status
+
+# remove job
+bash ./igorclient.sh end
+
+# remove cobbler distro/profile
+bash ./igorclient.sh extra_profile_remove "$BASENAME"
+
+[[ "x$LAST_STATE_SUCCESS" == "xdone" ]] || exit 1
+
+exit 0
