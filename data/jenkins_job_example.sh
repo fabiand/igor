f69b24fba5b88c47b8fcfb5f0e4f8495081c7652
Fabian Deutsch <fabiand@fedoraproject.org>
2012-06-01 10:00:34 +0200

    Not to restrictive on ISOs
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- data/jenkins_job_example.sh
+++ data/jenkins_job_example.sh
@@ -11,8 +11,11 @@
 [[ -z $BUILD_TAG ]] && exit 1
 
 # This is an artifact from a previous job
-[[ $(ls *.iso | wc -l) -gt 1 ]] && { echo More than one iso ; ls *.iso ; exit 1 ; }
-ISONAME=$(ls *.iso)
+ISONAME=$(ls *.iso | tail -n1)
+[[ $(ls *.iso | wc -l) -gt 1 ]] && { 
+    echo More than one iso ; 
+    ls *.iso ;
+}
 
 echo Using ISO '$ISONAME'
 
