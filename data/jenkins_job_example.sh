355b5e5fa755472e8d8c6d6922cd1766e52b2388
Fabian Deutsch <fabiand@fedoraproject.org>
2012-06-04 12:53:00 +0200

    jenkins: More on debug
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- data/jenkins_job_example.sh
+++ data/jenkins_job_example.sh
@@ -10,7 +10,8 @@
 [[ -z $ARTIFACTSARCHIVE ]] && exit 1
 [[ -z $BUILD_TAG ]] && exit 1
 
-drawline() { awk "BEGIN { while (a++<$1) s=s \"$2\"; print s }" ; }
+pyc() { python -c "$@" ; }
+drawline() { pyc "print('$2' * $1);" ; }
 highlight() { echo -e "\n$(drawline ${#1} -)\n$@\n$(drawline ${#1} -)\n" ; }
 
 # This is an artifact from a previous job
