f990b5569b3945caadf6afd4d6f6fc0624038ebc
Fabian Deutsch <fabiand@fedoraproject.org>
2012-06-19 18:57:08 -0400

    bootstrap: Rework bootstrap script even more
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- igor/data/client-bootstrap.sh
+++ igor/data/client-bootstrap.sh
@@ -4,13 +4,42 @@
 # TESTJOB - original kernelarg
 # APIURL  - the api base
 
+#
+# Vars
+#
+SESSION=${igor_cookie}
+CURRENT_STEP=${igor_current_step}
+TESTSUITE=${igor_testsuite}
+TMPDIR=$(mktemp -d)
+LOGFILE=${TMPDIR}/testcases.log
+
+# 
+# Functions
+#
+debug() { echo "$SESSION $(date) - $@" >&2 ; }
+debug_curl() { debug "Calling $1" ; curl --silent "$1" ; }
+api_url() { echo "${APIURL%/}/${1#/}" ; }
+api_call() { debug_curl $(api_url "$1") ; }
+step_succeeded() { api_call job/step/$SESSION/$CURRENT_STEP/success ; }
+step_failed()    { api_call job/step/$SESSION/$CURRENT_STEP/failed ; }
+add_artifact()
 {
-  SESSION=${igor_cookie}
-  CURRENT_STEP=${igor_current_step}
-  TESTSUITE=${igor_testsuite}
+  local DST=$1
+  local FILENAME=$2
+  [[ -z $DST || -z $FILENAME ]] && {
+    debug "Adding artifact: Destination '$DST' or filename '$FILENAME' missing." ; return 1 ;
+  }
+  debug "Adding artifact '$DST': '$FILENAME'"
+  URL=$(api_url "job/artifact/for/$SESSION/$DST")
+  curl --silent --request PUT --upload-file - "$URL" <"$FILENAME"
+}
 
-  debug() { echo "$SESSION $(date) - $@" >&2 ; }
 
+
+#
+# Run
+#
+{
   [[ -e /usr/libexec/ovirt-functions ]] &&
   {
     debug "Loading oVirt functions"
@@ -19,101 +48,64 @@
     . /etc/default/ovirt
   }
 
-  debug "Environment variables:"
-  env | sort
+  debug "Entering tmpdir $TMPDIR"
+  cd $TMPDIR
 
-  debug "Args:"
-  echo $@
+  debug "Fetching testsuite '$TESTSUITE' for session '$SESSION'"
+  api_call "job/testsuite/for/$SESSION" > testcases.tar.bz2
+  tar imxf testcases.tar.bz2
 
-  debug "Defining functions"
-  api_url() { echo "${APIURL%/}/${1#/}" ; }
+  debug "Running testcases"
+  cd testcases
 
-  api_call()
-  {
-    URL=$(api_url "$1")
-    debug "Calling $URL"
-    curl --silent "$URL"
-  }
+  typeset -fx debug debug_curl
+  typeset -fx api_url api_call add_artifact
+  typeset -fx step_succeeded step_failed
+  export APIURL SESSION CURRENT_STEP TESTSUITE
 
-  step_succeeded() { api_call job/step/$SESSION/$CURRENT_STEP/success ; }
-  step_failed()    { api_call job/step/$SESSION/$CURRENT_STEP/failed ; }
-
-  add_artifact()
-  {
-    DST=$1
-    FILENAME=$2
-    [[ -z $DST || -z $FILENAME ]] && {
-      debug "Adding artifact: Destination '$DST' or filename '$FILENAME' missing."
-      return 1
+  for TESTCASE in $(ls -1 . | sort -n)
+  do
+    [[ -d $TESTCASE ]] && {
+      debug "Is not testcase, a directory: $TESTCASE" ; continue
     }
-    debug "Adding artifact '$DST': '$FILENAME'"
-    URL=$(api_url "job/artifact/for/$SESSION/$DST")
-    curl --silent --request PUT --upload-file - "$URL" <"$FILENAME"
-  }
 
+    TESTCASESTEP=${TESTCASE/-*/}
+    [[ $TESTCASESTEP -lt $CURRENT_STEP ]] && {
+      debug "Skipping testcase $TESTCASE" ; continue
+    }
 
-  main()
-  {
-    debug "Fetching testsuite '$TESTSUITE' for session '$SESSION'"
-    api_call "job/testsuite/for/$SESSION" > testcases.tar.bz2
-    tar xf testcases.tar.bz2
+    debug "Running testcase $TESTCASE"
+    {
+      chmod a+x $TESTCASE
+      ./$TESTCASE
+      RETVAL=$?
+    }
+    debug "Testcase ended with: $RETVAL"
 
-    debug "Running testcases"
-    cd testcases
+    if [[ $RETVAL == 0 ]];
+    then
+      step_succeeded
+    else
+      step_failed
+      # We are not breaking here, because a failed testcase could be expected
+    fi
 
-    typeset -fx debug
-    typeset -fx api_url api_call add_artifact
-    typeset -fx step_succeeded step_failed
-    export SESSION CURRENT_STEP TESTSUITE
+    # Check if we are continuing
+    api_call job/status/$SESSION | grep '"state":' | grep -q '"running"' || {
+      debug "Testsuite is not running anymore"
+      break
+    }
 
-    for TESTCASE in *
-    do
-      [[ -d $TESTCASE ]] && {
-        debug "Is not testcase, a directory: $TESTCASE"
-        continue
-      }
-      TESTCASESTEP=${TESTCASE/-*/}
-      [[ $TESTCASESTEP -lt $CURRENT_STEP ]] && {
-        debug "Skipping testcase $TESTCASE"
-        continue
-      }
-      debug "Running testcase $TESTCASE"
+    CURRENT_STEP=$(($CURRENT_STEP + 1))
+  done
 
-      {
-        chmod a+x $TESTCASE
-        ./$TESTCASE
-        RETVAL=$?
-      }
+  # Add a summary
+  api_call job/status/$SESSION > "/tmp/job_status.json"
+  add_artifact "job_status.json.txt" "/tmp/job_status.json"
+} 2>&1 | tee $LOGFILE
 
-      [[ $(wc -l < /tmp/testcase.log) -gt 0 ]] && add_artifact "${TESTCASE#*-}.log" "/tmp/testcase.log"
+add_artifact "testcases.log" $LOGFILE
 
-      if [[ $RETVAL = 0 ]];
-      then
-        step_succeeded
-      else
-        step_failed
-        # We are not breaking here, because a failed testcase could be expected
-      fi
-
-      # Check if we are continuing
-      api_call job/status/$SESSION | grep '"state":' | grep -q '"running"' || {
-        debug "Testsuite is not running anymore"
-        break
-      }
-
-      CURRENT_STEP=$(($CURRENT_STEP + 1))
-    done
-
-    # Add a summary
-    api_call job/status/$SESSION > "/tmp/job_status.json"
-    add_artifact "job_status.json.txt" "/tmp/job_status.json"
-
-    debug "Finished testcases"
-    debug "Finished testsuite $TESTSUITE"
-  }
-
-  main
-
-} 2>&1 | tee testcases.log
+debug "Done"
 
 # vim: sw=2:
