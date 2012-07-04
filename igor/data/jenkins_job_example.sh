fce71e27bd00ddc5b85762ffe5b9fe378459ca75
Fabian Deutsch <fabiand@fedoraproject.org>
2012-07-04 15:06:30 +0200

    igor: Allow second route for testjob
    bottle is quite picky about double slashes (understandable).
    
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- igor/data/jenkins_job_example.sh
+++ igor/data/jenkins_job_example.sh
@@ -42,7 +42,7 @@ INITRD=tftpboot/initrd*
 KARGS=kargs
 echo $(sed -n "/APPEND/s/[[:space:]]*APPEND//p" tftpboot/pxelinux.cfg/default \
        | egrep -o "(root|ro|live|check|rhgb)[^[:space:]]*") > $KARGS
-echo " BOOTIF=link storage_init local_boot_trigger=$APIURL/testjob/{igor_cookie}" >> $KARGS
+echo " BOOTIF=link storage_init local_boot_trigger=${APIURL}testjob/{igor_cookie}" >> $KARGS
 bash ./igorclient.sh add_profile "$PROFILENAME" "$KERNEL" "$INITRD" "$KARGS"
 sudo rm -rvf tftpboot
 
