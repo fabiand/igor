522b376fc003a970b2e4890c55298afec65ee735
Fabian Deutsch <fabiand@fedoraproject.org>
2012-07-16 15:01:56 +0200

    client: Fix testplan timeout
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- igor/data/igorclient.sh
+++ igor/data/igorclient.sh
@@ -290,16 +290,17 @@ wait_testplan() # Wait until a testplan ended
   echo -n "Waiting "
   export DEBUG=""
   TIME_START=$(date +%s)
-  TIMEOUT=$(api testplans/$PNAME | grep timeout | tail -n1 | _filter_key timeout)
-  TIMEOUT=$(( $TIMEOUT * 2 )) # Higher timeout, because it can take time before the job ist actually started
   while true
   do
+    TIMEOUT=$(api testplans/$PNAME | grep timeout | tail -n1 | _filter_key timeout)
+    TIMEOUT=$(( $TIMEOUT * 2 )) # Higher timeout, because it can take time before the job ist actually started
+
     STATE=$(api testplans/$PNAME | grep status | tail -n1 | _filter_key status)
     echo $STATE | egrep -q "stopped" && break
     sleep $INTERVAL
     echo -n "."
     RUNTIME=$(( $(date +%s) - $TIME_START ))
-    [[ $RUNTIME -gt $TIMEOUT ]] && {
+    [[ ! -z $TIMEOUT && $RUNTIME -gt $TIMEOUT ]] && {
       echo "Timed out ($TIMEOUT)"
       break;
     }
